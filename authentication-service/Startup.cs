1964f847503dcfdb77f355da00f7a866314a89b8
swadhin <swadhin.mukherjee@cognizant.com>
2022-05-16 01:37:00 +0530

    Allow Cors

--- authentication-service/Startup.cs
+++ authentication-service/Startup.cs
@@ -22,6 +22,7 @@ namespace authentication_service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             var key = "Skilltrackerprivatekey";
             services.AddSingleton<IJwtAuthenticationManager>(new JwtAuthenticationManager(key));
@@ -58,6 +59,15 @@ namespace authentication_service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(
+            options => options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            );
+
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
